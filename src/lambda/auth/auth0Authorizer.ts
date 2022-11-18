2602df9479478f9e823fcc038eb03978ac20b306
kmanadkat <kmanadkat@gmail.com>
2022-11-18 22:53:18 +0530

    feat: Added Authentication

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,12 +1,24 @@
 import { CustomAuthorizerHandler, CustomAuthorizerEvent, CustomAuthorizerResult } from "aws-lambda";
+import * as AWS from 'aws-sdk';
+
+import { verify } from 'jsonwebtoken'
+import { JwtToken } from "../../auth/JwtToken";
+
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+
+const client = new AWS.SecretsManager()
+
+// Cached secret if a Lambda instance is reused
+let cachedSecret: string
 
 export const handler: CustomAuthorizerHandler = async (_event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
-    verifyToken(_event.authorizationToken)
+    const decodedToken = await verifyToken(_event.authorizationToken)
     console.log("User was authorized")
 
     return {
-      principalId: 'user',
+      principalId: decodedToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -37,7 +49,7 @@ export const handler: CustomAuthorizerHandler = async (_event: CustomAuthorizerE
   }
 }
 
-function verifyToken(authHeader: string) {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if(!authHeader) {
     throw new Error('No authorization header')
   }
@@ -49,9 +61,21 @@ function verifyToken(authHeader: string) {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  if(token !== '123'){
-    throw new Error('Invalid token')
-  }
+  const secretObject = await getSecret()
+  const secret = secretObject[secretField]
+
+  return verify(token, secret) as JwtToken
 
   // A request is authorized.
+}
+
+async function getSecret() {
+  if (cachedSecret) return JSON.parse(cachedSecret)
+
+  const data = await client.getSecretValue({
+    SecretId: secretId,
+  }).promise()
+
+  cachedSecret = data.SecretString
+  return JSON.parse(cachedSecret)
 }
